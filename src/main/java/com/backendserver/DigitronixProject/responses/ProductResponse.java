6bf0460891b2fd9b5b07aa829d1752ea2171a1c7
QuangDuyTrinh170201 <trinhquangduy3442@gmail.com>
2024-03-18 15:37:04 +0700

    fix img

--- src/main/java/com/backendserver/DigitronixProject/responses/ProductResponse.java
+++ src/main/java/com/backendserver/DigitronixProject/responses/ProductResponse.java
@@ -22,7 +22,7 @@ public class ProductResponse extends BaseResponse {
     private Long id;
     private String name;
     private Double price;
-    private Path img;
+    private UrlResource img;
     private int quantity;
 
     @JsonProperty("category_id")
@@ -34,10 +34,21 @@ public class ProductResponse extends BaseResponse {
 
     public static ProductResponse fromProduct(Product product) {
         Path imagePath = null;
+        UrlResource resource = null;
         if(product.getImg() == null){
             imagePath = Paths.get("uploads/notfound.jpg");
+            try {
+                resource = new UrlResource(imagePath.toUri());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
         }else{
             imagePath = Paths.get("uploads/"+product.getImg());
+            try {
+                resource = new UrlResource(imagePath.toUri());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
         }
         ProductResponse productResponse = null;
             productResponse = ProductResponse.builder()
@@ -45,7 +56,7 @@ public class ProductResponse extends BaseResponse {
                     .name(product.getProductName())
                     .price(product.getPrice())
                     .categoryId(product.getCategory().getId())
-                    .img(imagePath)
+                    .img(resource)
                     .quantity(product.getQuantity())
                     .tags(product.getTags().stream()
                             .map(Tag::getTagName)
