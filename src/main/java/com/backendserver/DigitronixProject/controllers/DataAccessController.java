aed8b8f3d3802b4f9fe5b3728947b3ffb7807705
QuangDuyTrinh170201 <trinhquangduy3442@gmail.com>
2024-04-21 20:46:06 +0700

    add role into data access

--- src/main/java/com/backendserver/DigitronixProject/controllers/DataAccessController.java
+++ src/main/java/com/backendserver/DigitronixProject/controllers/DataAccessController.java
@@ -15,33 +15,30 @@ import java.util.List;
 @RestController
 @RequestMapping("${api.prefix}/data_accesses")
 @RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ROLE_DIRECTOR', 'ROLE_INVENTORY_MANAGER')")
 public class DataAccessController {
 
     private final DataAccessService dataAccessService;
 
     @PostMapping
-    @PreAuthorize("hasRole('ROLE_DIRECTOR')")
     public ResponseEntity<DataAccess> createDataAccess(@RequestBody DataAccessDTO dataAccessDTO) {
         DataAccess newDataAccess = dataAccessService.createDataAccess(dataAccessDTO);
         return new ResponseEntity<>(newDataAccess, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_DIRECTOR')")
     public ResponseEntity<DataAccess> getDataAccessById(@PathVariable("id") Long id) {
         DataAccess dataAccess = dataAccessService.getById(id);
         return new ResponseEntity<>(dataAccess, HttpStatus.OK);
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('ROLE_DIRECTOR')")
     public ResponseEntity<List<DataAccess>> getAllDataAccesses() {
         List<DataAccess> dataAccesses = dataAccessService.getAll();
         return new ResponseEntity<>(dataAccesses, HttpStatus.OK);
     }
 
     @DeleteMapping
-    @PreAuthorize("hasRole('ROLE_DIRECTOR')")
     public ResponseEntity<String> deleteAllDataAccesses() {
         String message = dataAccessService.deleteAll();
         return new ResponseEntity<>(message, HttpStatus.OK);
